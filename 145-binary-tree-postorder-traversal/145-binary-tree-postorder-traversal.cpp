09527ee9e3ea8ac7dda17364085a286f6901086f
Aman singh <84977301+pratapsinghaman12@users.noreply.github.com>
2022-02-16 16:30:31 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (87.24%) - LeetHub

---
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+      void aman_in(TreeNode* root, vector<int> &v)
+    {
+        if(root!=NULL)
+        {
+           
+            
+            aman_in(root->left,v);
+            
+            aman_in(root->right,v);
+            
+             v.push_back(root->val);
+            
+        }
+        
+        
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+         vector<int>v;
+        aman_in(root,v);
+        return v;
+    }
+};
