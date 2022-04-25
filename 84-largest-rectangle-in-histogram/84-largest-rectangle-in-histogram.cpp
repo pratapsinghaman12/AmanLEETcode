8fd20fcc02419549a6c191e7a578313fd4205719
Aman singh <84977301+pratapsinghaman12@users.noreply.github.com>
2022-04-25 23:29:14 +0530

    Time: 181 ms (81.86%), Space: 75.3 MB (91.20%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.cpp
@@ -1,66 +1,27 @@
 class Solution {
 public:
-    
-    
-    
-    
-    vector<int> aman_nse(vector<int> &arr,int n)
-    {
-         vector<int>nse(n,n);
-          stack<int>st;
-         st.push(n-1);
-         for(int i=n-2;i>=0;i--)
-         {
-             while(!st.empty()&&arr[i]<=arr[st.top()])
-             {
-                 st.pop();
-             }
-             if(!st.empty())
-             {
-                 nse[i]=st.top();
-             }
-             st.push(i);
-         }
-         return nse;
-         
-    }
-    
-     vector<int> aman_pse(vector<int> &arr,int n)
-    {
-         vector<int>pse(n,-1);
-          stack<int>st;
-          st.push(0);
-          for(int i=1;i<n;i++)
-          {
-              while(!st.empty()&&arr[st.top()]>=arr[i])
-              {
-                  st.pop();
-              }
-              if(!st.empty())
-              {
-                  pse[i]=st.top();
-              }
-              st.push(i);
-          }
-          return pse;
-    }
-    
-    
-    
-    
-    
-    
-    
-    
     int largestRectangleArea(vector<int>& arr) {
         int n=arr.size();
-        vector<int>nse=aman_nse(arr,n);
-        vector<int>pse=aman_pse(arr,n);
-        int res=INT_MIN;
-       for(int i=0;i<n;i++)
-       {
-          res=max(res,arr[i]*(nse[i]-pse[i]-1)); 
-       }
-       return res;
+        stack<int>st;
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            while(st.empty()==false&&arr[st.top()]>=arr[i])
+            {
+                int x= st.top();
+                st.pop();
+                int curr= arr[x]*(st.empty()?i:(i-st.top()-1));
+                ans=max(ans,curr);
+            }
+            st.push(i);
+        }
+        while(st.empty()==false)
+        {
+            int x= st.top();
+            st.pop();
+            int curr= arr[x]*(st.empty()?n:(n-st.top()-1));
+            ans=max(ans,curr);
+        }
+        return ans;
     }
 };
