53ec18e316cc7682ac36ffc9e2c59100ae39ea30
Aman singh <84977301+pratapsinghaman12@users.noreply.github.com>
2022-05-01 01:20:12 +0530

    Time: 150 ms (25.89%), Space: 54.9 MB (6.33%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int>st(nums.begin(),nums.end());
+        if(st.size()==nums.size())
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+};
