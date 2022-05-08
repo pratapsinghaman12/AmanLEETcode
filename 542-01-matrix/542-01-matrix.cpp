9d5654e0642de40909987c7445053acda277eb9b
Aman singh <84977301+pratapsinghaman12@users.noreply.github.com>
2022-05-08 21:38:31 +0530

    Time: 151 ms (20.31%), Space: 30 MB (68.82%) - LeetHub

---
+++ 542-01-matrix/542-01-matrix.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        queue<pair<int,int>>ans;
+        for(int i=0;i<mat.size();i++){
+         for(int j=0;j<mat[0].size();j++){
+             if(mat[i][j]==0)
+                 ans.push({i,j});
+             else 
+                 mat[i][j]=-1;
+         }
+        }
+        int dx[4]={0,0,1,-1};
+        int dy[4]={1,-1,0,0};
+        while(ans.size()>0){
+            auto it=ans.front();
+            ans.pop();
+        for(int i=0;i<4;i++){
+            int coly=it.second+dy[i];
+            int colx=it.first+dx[i];
+         if(colx>=0&&coly>=0&&colx<mat.size()&&coly<mat[0].size()&&mat[colx][coly]<0)
+         {
+             ans.push({colx,coly});
+             mat[colx][coly]=mat[it.first][it.second]+1;
+         }
+        }
+        }
+        
+        return mat;
+    }
+    
+};
