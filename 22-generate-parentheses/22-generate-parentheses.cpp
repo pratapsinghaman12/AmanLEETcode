e4b779d6d6538504d7913767c2789602855f7459
Aman singh <84977301+pratapsinghaman12@users.noreply.github.com>
2022-05-11 01:39:21 +0530

    Time: 3 ms (83.66%), Space: 11.5 MB (76.69%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<string>ans;
+    void aman(string &s,int open,int close)
+    {
+        if(open==0&&close==0)
+        {
+            ans.push_back(s);
+            return;
+        }
+        if(open>0)
+        {
+            s.push_back('(');
+            aman(s,open-1,close);
+            s.pop_back();
+        }
+        if(close>0)
+        {
+            if(open<close)
+            {
+                s.push_back(')');
+                aman(s,open,close-1);
+                s.pop_back();
+            }
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        string s="";
+        aman(s,n,n);
+        return ans;
+    }
+};
