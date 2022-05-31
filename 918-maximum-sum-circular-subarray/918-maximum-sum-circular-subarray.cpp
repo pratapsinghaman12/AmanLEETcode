a0014cb5de045c4a5cc449970bcea0d2883a499b
Aman singh <84977301+pratapsinghaman12@users.noreply.github.com>
2022-06-01 03:04:41 +0530

    Time: 92 ms (41.39%), Space: 39.8 MB (71.00%) - LeetHub

--- 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.cpp
+++ 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
-    int amanKadane(vector<int>arr,int n)
-    {
-        int res=arr[0];
-        int curr_sum=arr[0];
-        for(int i=1;i<n;i++)
-        {
-            curr_sum=max(curr_sum+arr[i],arr[i]);
-            res=max(res,curr_sum);
-        }
-        return res;
-    }
+    int aman(vector<int>&nums)
+	{
+		int n=nums.size();
+		int curr_sum=nums[0];
+		int ans=nums[0];
+		for(int i=1;i<n;i++)
+		{
+			curr_sum+=nums[i];
+			curr_sum=max(curr_sum,nums[i]);
+			ans=max(ans,curr_sum);
+		}
+		return ans;
+	}
     int maxSubarraySumCircular(vector<int>& nums) {
         int n=nums.size();
-        int sum=0;
-        int first=amanKadane(nums,n);
-        if(first<0)
-        {
-            return first;
-        }
-        for(int i=0;i<n;i++)
-        {
-           sum+=nums[i];
-            nums[i]=nums[i]*-1;
-        }
-        
-        int second= sum+amanKadane(nums,n);
-        return max(first,second);
+		int total=0;
+		int normal=aman(nums);
+		if(normal<0)
+		{
+			return normal;
+		}
+		for(int i=0;i<n;i++)
+		{
+			total+=nums[i];
+			nums[i]=nums[i]*-1;
+		}
+		
+		
+		
+		int curr=aman(nums);
+		return max(normal,total+curr);
     }
 };
