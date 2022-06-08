86858e66a5d1842567a9489c9b35baae2d228ecc
Aman singh <84977301+pratapsinghaman12@users.noreply.github.com>
2022-06-08 11:39:09 +0530

    Time: 44 ms (12.82%), Space: 18 MB (64.46%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -2,16 +2,34 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n=nums.size();
-        int sum= n*(n+1)/2;
-        int curr_sum=0;
+        bool aman=false;
         for(int i=0;i<n;i++)
         {
-            curr_sum+=nums[i];
+            int x= abs(nums[i]);
+            if(x==0||x==n+1)
+            {
+                aman=true;
+            }
+            else if(nums[x-1]==0)
+            {
+                nums[x-1]=(n+1)*-1;
+            }
+            else
+            {
+                nums[x-1]=nums[x-1]*-1;
+            }  
         }
-        if(sum==curr_sum)
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]>=0)
+            {
+                return i+1;
+            }
+        }
+        if(aman==false)
         {
             return 0;
         }
-        return sum-curr_sum;
+        return -1;
     }
 };
