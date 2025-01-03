393a7f133f57a0e88ecefeb04f087098ec188c6e
Aman singh <84977301+pratapsinghaman12@users.noreply.github.com>
2025-01-03 17:44:59 +0530

    Time: 3 ms (94.63%), Space: 61.2 MB (93.92%) - LeetHub

---
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long zeroFilledSubarray(int[] nums) {
+        long count = 0; 
+        long ans = 0;  
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                count++;
+            } else {
+                ans += (count * (count + 1)) / 2; 
+                count = 0; 
+            }
+        }
+        
+        ans += (count * (count + 1)) / 2;
+        return ans;
+    }
+}
