e19ceefb4957159f6157a49b6ca85697e9676ac5
Aman singh <84977301+pratapsinghaman12@users.noreply.github.com>
2025-01-09 15:55:00 +0530

    Time: 3 ms (96.09%), Space: 44.9 MB (7.19%) - LeetHub

---
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int pathSum(TreeNode root, int targetSum) {
+        HashMap<Long, Integer> prefixSum = new HashMap<>();
+        prefixSum.put(0L, 1); 
+        return dfs(root, 0L, targetSum, prefixSum);
+    }
+
+    private int dfs(TreeNode node, long currentSum, int targetSum, HashMap<Long, Integer> prefixSum) {
+        if (node == null) {
+            return 0;
+        }
+
+        currentSum += node.val;
+
+        
+        int count = prefixSum.getOrDefault(currentSum - targetSum, 0);
+
+        prefixSum.put(currentSum, prefixSum.getOrDefault(currentSum, 0) + 1);
+
+        
+        count += dfs(node.left, currentSum, targetSum, prefixSum);
+        count += dfs(node.right, currentSum, targetSum, prefixSum);
+
+        
+        prefixSum.put(currentSum, prefixSum.get(currentSum) - 1);
+
+        return count;
+    }
+}
